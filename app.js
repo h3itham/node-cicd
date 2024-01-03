529bd809ba981f8b6ff65a7f6e6fc85a9f04b668
h3itham <eng.haithamelabd@gmail.com>
2024-01-03 06:21:32 +0200

    Update app

--- app.js
+++ app.js
@@ -1,9 +1,40 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+
 const PORT = process.env.PORT || 3000;
 
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
 app.get('/', (req, res) => {
-  res.send('Hi, Haitham!');
+  // Sending HTML response with centered content
+  res.send(`
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <title>Centered Content</title>
+      <style>
+        body {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          height: 100vh;
+          margin: 0;
+        }
+        .content {
+          text-align: center;
+        }
+      </style>
+    </head>
+    <body>
+      <div class="content">
+        <h1>Hi, Haitham!</h1>
+        <img src="/images/wal.png" alt="Sample Image" style="width: 300px; height: auto;" />
+      </div>
+    </body>
+    </html>
+  `);
 });
 
 app.listen(PORT, () => {
